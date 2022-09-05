9dce8ed32729c40cb148d36edacc5d16991e736b
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2022-09-05 21:26:54 +0200

    Initial commit

---
+++ src/RazorBlade.Analyzers/RazorBladeSourceGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace RazorBlade.Analyzers;
+
+[Generator]
+public class RazorBladeSourceGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+
+    }
+}
