fe59e8959b9e7ee10ab64daac9d262d4e1952485
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2022-09-11 16:15:07 +0200

    Add Html.Encode

--- src/RazorBlade.Tests/HtmlHelperTests.cs
+++ src/RazorBlade.Tests/HtmlHelperTests.cs
@@ -12,4 +12,15 @@ public class HtmlHelperTests
         var result = HtmlHelper.Instance.Raw("&<>");
         result.ToHtmlString().ShouldEqual("&<>");
     }
+
+    [Test]
+    [TestCase("", "")]
+    [TestCase("foo", "foo")]
+    [TestCase("&foo", "&amp;foo")]
+    [TestCase("foo&", "foo&amp;")]
+    [TestCase("foo & bar < baz > foobar", "foo &amp; bar &lt; baz &gt; foobar")]
+    public void should_html_encode_string(string input, string expectedOutput)
+    {
+        HtmlHelper.Instance.Encode(input).ShouldEqual(expectedOutput);
+    }
 }
--- src/RazorBlade/HtmlHelper.cs
+++ src/RazorBlade/HtmlHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
 
 namespace RazorBlade;
 
@@ -21,4 +23,49 @@ public sealed class HtmlHelper
     /// <param name="value">The HTML markup.</param>
     public IHtmlString Raw(object? value)
         => new HtmlString(value?.ToString());
+
+    /// <summary>
+    /// HTML-encodes the provided value.
+    /// </summary>
+    /// <param name="value">Value to HTML-encode.</param>
+    public string Encode(object? value)
+    {
+        var valueString = value?.ToString();
+        if (valueString is null or "")
+            return string.Empty;
+
+#if NET6_0_OR_GREATER
+        var valueSpan = valueString.AsSpan();
+        var sb = new StringBuilder();
+
+        while (true)
+        {
+            var idx = valueSpan.IndexOfAny('&', '<', '>');
+            if (idx < 0)
+                break;
+
+            if (idx != 0)
+                sb.Append(valueSpan[..idx]);
+
+            sb.Append(valueSpan[idx] switch
+            {
+                '&' => "&amp;",
+                '<' => "&lt;",
+                '>' => "&gt;",
+                _   => throw new InvalidOperationException()
+            });
+
+            valueSpan = valueSpan[(idx + 1)..];
+        }
+
+        if (valueSpan.Length != 0)
+            sb.Append(valueSpan);
+
+        return sb.ToString();
+#else
+        return valueString.Replace("&", "&amp;")
+                          .Replace("<", "&lt;")
+                          .Replace(">", "&gt;");
+#endif
+    }
 }
