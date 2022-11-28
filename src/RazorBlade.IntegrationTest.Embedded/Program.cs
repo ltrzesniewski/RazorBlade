0c2fe5c75f14dbaddbf170b1b4cc271bb8584269
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2022-11-29 00:19:10 +0100

    Add integration test for embedded library

---
+++ src/RazorBlade.IntegrationTest.Embedded/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RazorBlade.IntegrationTest;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var template = new TestTemplate { Name = "World" };
+        var result = template.Render();
+
+        Console.WriteLine(result);
+    }
+}
